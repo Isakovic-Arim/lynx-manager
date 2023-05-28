8644096e9ee1dec8a9a25d224397a599e8716449
isakovic-arim <arim.isakovic2005@gmail.com>
2023-05-28 11:54:31 +0200

    Test basic operations with mongodb realm

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,25 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
+
+import Realm from 'realm';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const app = express();
-const client = new MongoClient('mongodb://localhost:27017');
+const realm = new Realm.App({ id:  process.env.APP_ID!});
+const client = new MongoClient(process.env.URI!);
 
 async function run() {
   try {
+    realm.currentUser?.logOut();
+    const user: Realm.User = await realm.logIn(Realm.Credentials.anonymous());
+    if (realm.currentUser !== null) {
+      const mongodb = realm.currentUser.mongoClient("mongodb-atlas");
+      console.log(mongodb);
+      const reviews = mongodb.db("sample_airbnb").collection("listingsAndReviews");
+      console.log(await reviews.count());
+    }
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -19,6 +32,7 @@ async function run() {
 }
 run().catch(console.dir);
 
+const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
